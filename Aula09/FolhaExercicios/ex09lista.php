e2d23c2094031c4444f75c8cb896bce9218ce275
gustavoherdt10 <gusherdt@gmail.com>
2024-10-08 19:57:39 -0300

    Envia

--- Aula09/FolhaExercicios/ex09lista.php
+++ Aula09/FolhaExercicios/ex09lista.php
@@ -10,10 +10,7 @@
     <h2>Opções de Parcelamento da Moto com Juros Compostos</h2>
 
     <?php
-    // Valor da moto à vista
     $valorAVista = 8654.00;
-
-    // Taxas de juros para cada prazo (iniciando com 2% e aumentando 0,3% a cada nível)
     $taxasDeJuros = [
         24 => 0.02,  // 2,0% para 24 vezes
         36 => 0.023, // 2,3% para 36 vezes
@@ -21,10 +18,7 @@
         60 => 0.029  // 2,9% para 60 vezes
     ];
 
-    // Exibindo as opções de parcelamento
     foreach ($taxasDeJuros as $parcelas => $taxa) {
-        // Fórmula de Juros Compostos: M = C * (1 + i)^t
-        // C = valor à vista, i = taxa de juros, t = número de parcelas
         $valorTotal = $valorAVista * pow((1 + $taxa), $parcelas);
         $valorParcela = $valorTotal / $parcelas;
 
